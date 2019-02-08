5daa70585b94bde76ae1b20e4b12727ba47fd25b
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-02-08 11:35:46 +0700

    Update and rename shapes/classes/Triangle.java to Shapes/src/shapesPackage/classes/Triangle.java

--- Shapes/src/shapesPackage/classes/Triangle.java
+++ Shapes/src/shapesPackage/classes/Triangle.java
@@ -18,32 +18,30 @@ public class Triangle implements Shape {
         this.x3 = x3;
         this.y3 = y3;
     }
-
-    public double getWidth() {
+    
+    public double getTriangleLength(double x1, double x2, double x3) {
         return (Math.max(Math.max(x1, x2), x3) - Math.min(Math.min(x1, x2), x3));
     }
+    
+    @Override
+    public double getWidth() {
+        return getTriangleLength(x1, x2, x3);
+    }
 
+    @Override
     public double getHeight() {
-        return (Math.max(Math.max(y1, y2), y3) - Math.min(Math.min(y1, y2), y3));
+        return getTriangleLength(y1, y2, y3);;
     }
-
+    
+    @Override
     public double getArea() {
-        try {
-            return 0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
-        } catch ( NullPointerException e ) {
-            System.out.println("Множество фигур не должно быть пустым! ");
-            return 0;
-        }
+        return 0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));       
     }
-
-    public double getPerimeter() {
-        try {
-            return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2)) +
+    
+    @Override
+    public double getPerimeter() 
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2)) +
                     Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
-        } catch ( NullPointerException e ) {
-            System.out.println("Множество фигур не должно быть пустым! ");
-            return 0;
-        }
     }
 
     @Override
@@ -67,7 +65,6 @@ public class Triangle implements Shape {
 
     @Override
     public boolean equals(Object object) {
-        try {
             if (object == this) {
                 return true;
             }
@@ -78,11 +75,7 @@ public class Triangle implements Shape {
             Triangle triangle = (Triangle) object;
 
             return (this.x1 == triangle.x1 && this.y1 == triangle.y1 && this.x2 == triangle.x2 && this.y2 == triangle.y2 && this.x3 == triangle.x3 && this.y3 == triangle.y3);
-
-        } catch ( RuntimeException e ) {
-            e.getMessage();
-            return false;
-        }
+       
     }
 }
 
