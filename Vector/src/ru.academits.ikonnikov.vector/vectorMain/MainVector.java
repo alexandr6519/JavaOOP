3dc3721c86ab1c0cabc8ad385b3a6e8d5ff52758
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-02-12 13:39:32 +0700

    Create MainVector.java
    initial commit of task Vector

---
+++ Vector/src/ru.academits.ikonnikov.vector/vectorMain/MainVector.java
@@ -0,0 +1,54 @@
+package ru.academits.ikonnikov.vector.vectorMain;
+
+import ru.academits.ikonnikov.vector.classes.Vector;
+
+import static ru.academits.ikonnikov.vector.classes.Vector.add;
+import static ru.academits.ikonnikov.vector.classes.Vector.multiplyScalar;
+import static ru.academits.ikonnikov.vector.classes.Vector.subtract;
+
+public class MainVector {
+    public static void main(String[] args) throws NullPointerException {
+        double[] array1 = {11.0, 2.5, 5.1};
+        Vector v1 = new Vector(array1);
+        System.out.println("Vector v1 : " + v1.toString());
+        System.out.printf("   The length of vector v1 = %.2f %n", v1.getLength());
+
+        double[] array2 = {-1.0, -4.2, 2.2, 5.6};
+        Vector v2 = new Vector(array2);
+        System.out.println("Vector v2 : " + v2.toString());
+        System.out.printf("   The length of vector v2 = %.2f %n", v2.getLength());
+
+        Vector v3 = new Vector(5, array2);
+        System.out.println("Vector v3 : " + v3.toString());
+        System.out.println("     Size of vector v3 = " + v3.getSize());
+        System.out.println("The result of turn of vector v3 : " + v3.turn().toString());
+
+        double[] array3 = {1.0, 2.5};
+        Vector v4 = new Vector(array3);
+        System.out.println("Vector v4 : " + v4.toString());
+
+        v4.setComponent(0, -2.5);
+        v4.setComponent(1, 1.3);
+        System.out.println(" After changing of values vector v4 : " + v4.toString());
+
+        Vector v5 = new Vector(v4);
+
+        System.out.println("Vector v5 is copy of v4 : " + v5.toString());
+
+        System.out.println("The result of addition of vectors v1 and v2 by nonstatic method is: " + v1.add(v2).toString());
+        System.out.println("The result of addition of vectors v1 and v2 by static method is: " + add(v1, v2).toString());
+        System.out.println("The result of subtraction of vector v2 and v1 by nonstatic method is: " + v1.subtract(v2).toString());
+        System.out.println("The result of subtraction of vector v2 and v1 by static method is: " + subtract(v1, v2).toString());
+        System.out.println("The result of multiplication of vector v3 by scalar (3,30) is: " + v3.multiplyByScalar(3.3).toString());
+        System.out.println("The result of multiplication of vector v3 by scalar (-0,50) is: " + v3.multiplyByScalar(-0.5).toString());
+        System.out.printf("The result of scalar multiplication of vectors v2 and v3 = %.2f %n", multiplyScalar(v2, v3));
+        System.out.println("The result of comparing vectors v2 and v4 is: " + v4.equals(v2));
+        System.out.println("The result of comparing vectors v4 and v5 is: " + v4.equals(v5));
+
+        System.out.printf("  The first component of vector v4 = %.2f %n  The second component of vector v4 = %.2f %n", v4.getComponent(0), v4.getComponent(1));
+        System.out.printf("The hashCode of vector v2 = %d %n", v2.hashCode());
+        System.out.printf("The hashCode of vector v4 = %d %n", v4.hashCode());
+        Vector v6 = new Vector(-2);
+    }
+}
+
