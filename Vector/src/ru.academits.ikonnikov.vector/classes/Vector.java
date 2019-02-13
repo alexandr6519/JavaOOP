4b8e76324da9a6e20b25f6715df9189d6b55800e
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-02-13 23:21:13 +0700

    Add files via upload
    correcting of methods

--- Vector/src/ru.academits.ikonnikov.vector/classes/Vector.java
+++ Vector/src/ru.academits.ikonnikov.vector/classes/Vector.java
@@ -1,259 +1,137 @@
 package ru.academits.ikonnikov.vector.classes;
 
 import java.lang.IllegalArgumentException;
+import java.lang.IndexOutOfBoundsException;
 
 public class Vector {
-    private int n;
-    public double[] array;
-    private final double EPSILON = 1.0e-10;
+    public double[] arrayDoubles;
 
     public Vector(int n) {
         if (n < 1) {
             throw new IllegalArgumentException("The parameter 'n' must be > 0!");
         }
 
-        this.n = n;
-        this.array = new double[n];
-
-        for (int i = 0; i < n; i++) {
-            this.array[i] = 0.0;
-        }
+        this.arrayDoubles = new double[n];
     }
 
     public Vector(Vector vector) {
-        if (vector.n < 1) {
-            throw new IllegalArgumentException("The length of vector must be > 0!");
-        }
-
-        this.n = vector.n;
-        this.array = vector.array;
+        int n = vector.arrayDoubles.length;
+        this.arrayDoubles = new double[n];
+        System.arraycopy(vector.arrayDoubles, 0, this.arrayDoubles, 0, n);
     }
 
-    public Vector(double[] array) {
-        if (array.length < 1) {
-            throw new IllegalArgumentException("This array of doubles is empty!");
-        }
-
-        this.n = array.length;
-        this.array = array;
+    public Vector(double[] arrayDoubles) {
+        int n = arrayDoubles.length;
+        this.arrayDoubles = new double[n];
+        System.arraycopy(arrayDoubles, 0, this.arrayDoubles, 0, n);
     }
 
-    public Vector(int n, double[] array) {
+    public Vector(int n, double[] arrayDoubles) {
         if (n < 1) {
             throw new IllegalArgumentException("The parameter 'n' must be > 0!");
         }
 
-        if (array.length < 1) {
-            throw new IllegalArgumentException("This array of doubles is empty!");
-        }
-
-        this.n = n;
-        this.array = new double[n];
-        for (int i = 0; i < this.n; i++) {
-            if (i < array.length) {
-                this.array[i] = array[i];
-            } else {
-                this.array[i] = 0;
-            }
-        }
+        this.arrayDoubles = new double[n];
+        int minSize = Math.min(n, arrayDoubles.length);
+        System.arraycopy(arrayDoubles, 0, this.arrayDoubles, 0, minSize);
     }
 
     public int getSize() {
-        return this.n;
+        return this.arrayDoubles.length;
     }
 
     public Vector add(Vector vector) {
-        if (vector.n < 1) {
-            throw new IllegalArgumentException("The length of vector must be > 0!");
+        int m = vector.arrayDoubles.length;
+        int n = this.arrayDoubles.length;
+        int minSize = Math.min(n, m);
+        int maxSize = Math.max(n, m);
+
+        Vector v = new Vector(maxSize, this.arrayDoubles );
+
+        for (int i = 0; i < minSize; i++) {
+            v.arrayDoubles[i] = this.arrayDoubles[i] + vector.arrayDoubles[i];
         }
 
-        int m = vector.array.length;
-        int maxSize = m > this.n ? m : this.n;
-        Vector resultVector = new Vector(maxSize);
-
-        if (m >= this.n) {
-            for (int i = 0; i < m; i++) {
-                if (i < this.n) {
-                    resultVector.array[i] = this.array[i] + vector.array[i];
-                } else {
-                    resultVector.array[i] = vector.array[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < this.n; i++) {
-                if (i < m) {
-                    resultVector.array[i] = this.array[i] + vector.array[i];
-                } else {
-                    resultVector.array[i] = this.array[i];
-                }
-            }
+        if (m >= n) {
+            System.arraycopy(vector.arrayDoubles, n, v.arrayDoubles, n, m - n);
         }
-        return resultVector;
+
+        this.arrayDoubles = new double[maxSize];
+        System.arraycopy(v.arrayDoubles, 0, this.arrayDoubles, 0, maxSize);
+
+        return this;
     }
 
     public Vector subtract(Vector vector) {
-        if (vector.n < 1) {
-            throw new IllegalArgumentException("The length of vector must be > 0!");
-        }
-
-        int m = vector.array.length;
-        int maxSize = m > this.n ? m : this.n;
-        Vector resultVector = new Vector(maxSize);
-
-        if (m >= this.n) {
-            for (int i = 0; i < m; i++) {
-                if (i < this.n) {
-                    resultVector.array[i] = this.array[i] - vector.array[i];
-                } else {
-                    resultVector.array[i] = -vector.array[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < this.n; i++) {
-                if (i < m) {
-                    resultVector.array[i] = this.array[i] - vector.array[i];
-                } else {
-                    resultVector.array[i] = this.array[i];
-                }
-            }
-        }
-        return resultVector;
+        Vector vector1 = new Vector(vector);
+        return this.add(vector1.turn());
     }
 
     public Vector multiplyByScalar(double scalar) {
-        Vector resultVector = new Vector(this.n);
-
-        for (int i = 0; i < this.n; i++) {
-            if (Math.abs(this.array[i] * scalar) < EPSILON) {
-                resultVector.array[i] = 0;
-            } else {
-                resultVector.array[i] = this.array[i] * scalar;
-            }
+        for (int i = 0; i < this.arrayDoubles.length; i++) {
+            this.arrayDoubles[i] = this.arrayDoubles[i] * scalar;
         }
-
-        return resultVector;
+        return this;
     }
 
     public Vector turn() {
-        Vector resultVector = new Vector(this.n);
-
-        for (int i = 0; i < this.n; i++) {
-            if (Math.abs(this.array[i]) > EPSILON) {
-                resultVector.array[i] = -this.array[i];
-            } else {
-                resultVector.array[i] = 0.0;
-            }
-        }
-        return resultVector;
+        return this.multiplyByScalar(-1);
     }
 
     public double getLength() {
         double sum = 0;
 
-        for (int i = 0; i < this.n; i++) {
-            sum += this.array[i] * this.array[i];
+        for (double itemArrayDouble : this.arrayDoubles) {
+            sum += itemArrayDouble * itemArrayDouble;
         }
         return Math.sqrt(sum);
     }
 
     public double getComponent(int i) {
-        if (i < 0 || i >= this.array.length) {
-            throw new IllegalArgumentException("The index of array isn't correct!");
+        if (i < 0 || i >= this.arrayDoubles.length) {
+            throw new IndexOutOfBoundsException("The index of array isn't correct!");
         }
-        return this.array[i];
+        return this.arrayDoubles[i];
     }
 
-    public void setComponent(int i, double component) {
-        if (i < 0 || i >= this.array.length) {
-            throw new IllegalArgumentException("The index of array isn't correct!");
+    public void setComponent(int i, double arrayComponent) {
+        if (i < 0 || i >= this.arrayDoubles.length) {
+            throw new IndexOutOfBoundsException("The index of array isn't correct!");
         }
-        this.array[i] = component;
+        this.arrayDoubles[i] = arrayComponent;
     }
 
     public static Vector add(Vector vector1, Vector vector2) {
-        if (vector1.n < 1 || vector2.n < 1) {
-            throw new IllegalArgumentException("The length of vectors must be > 0!");
-        }
-
-        int m = vector1.array.length;
-        int n = vector2.array.length;
-        int maxSize = m > n ? m : n;
-        Vector resultVector = new Vector(maxSize);
-
-        if (m >= n) {
-            for (int i = 0; i < m; i++) {
-                if (i < n) {
-                    resultVector.array[i] = vector1.array[i] + vector2.array[i];
-                } else {
-                    resultVector.array[i] = vector1.array[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < n; i++) {
-                if (i < m) {
-                    resultVector.array[i] = vector1.array[i] + vector2.array[i];
-                } else {
-                    resultVector.array[i] = vector2.array[i];
-                }
-            }
-        }
-        return resultVector;
+        Vector vector3 = new Vector(vector1);
+        return vector3.add(vector2);
     }
 
     public static Vector subtract(Vector vector1, Vector vector2) {
-        if (vector1.n < 1 || vector2.n < 1) {
-            throw new IllegalArgumentException("The length of vectors must be > 0!");
-        }
-
-        int m = vector1.array.length;
-        int n = vector2.array.length;
-        int maxSize = m > n ? m : n;
-        Vector resultVector = new Vector(maxSize);
-
-        if (m >= n) {
-            for (int i = 0; i < m; i++) {
-                if (i < n) {
-                    resultVector.array[i] = vector1.array[i] - vector2.array[i];
-                } else {
-                    resultVector.array[i] = vector1.array[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < n; i++) {
-                if (i < m) {
-                    resultVector.array[i] = vector1.array[i] - vector2.array[i];
-                } else {
-                    resultVector.array[i] = -vector2.array[i];
-                }
-            }
-        }
-        return resultVector;
+        Vector vector3 = new Vector(vector1);
+        Vector vector4 = new Vector(vector2);
+        return vector3.subtract(vector4);
     }
 
     public static double multiplyScalar(Vector vector1, Vector vector2) {
-        if (vector1.n < 1 || vector2.n < 1) {
-            throw new IllegalArgumentException("The length of vectors must be > 0!");
-        }
-
-        int m = vector1.array.length;
-        int n = vector2.array.length;
-        int minSize = m <= n ? m : n;
+        int minSize = Math.min(vector1.arrayDoubles.length, vector2.arrayDoubles.length);
         double result = 0;
 
         for (int i = 0; i < minSize; i++) {
-            result += vector1.array[i] * vector2.array[i];
+            result += vector1.arrayDoubles[i] * vector2.arrayDoubles[i];
         }
         return result;
     }
 
     @Override
     public String toString() {
-        String result = "{";
-        for (int i = 0; i < this.n - 1; i++) {
-            String str = String.format(" %.2f ;", this.array[i]);
-            result = result.concat(str);
+        int n = this.arrayDoubles.length;
+        StringBuilder result = new StringBuilder("{");
+
+        for (int i = 0; i < n - 1; i++) {
+            String str = String.format(" %.2f ;", this.arrayDoubles[i]);
+            result = result.append(str);
         }
-        return result.concat(String.format(" %.2f }", this.array[this.n - 1]));
+        return result.append(String.format(" %.2f }", this.arrayDoubles[n - 1])).toString();
     }
 
     @Override
@@ -267,14 +145,15 @@ public class Vector {
         }
 
         Vector vector = (Vector) object;
+        int n = this.arrayDoubles.length;
 
-        if (vector.n != this.n) {
+        if (vector.arrayDoubles.length != n) {
             return false;
-        } else {
-            for (int i = 0; i < n; i++) {
-                if (Math.abs(this.array[i] - vector.array[i]) > EPSILON) {
-                    return false;
-                }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (this.arrayDoubles[i] != vector.arrayDoubles[i]) {
+                return false;
             }
         }
         return true;
@@ -285,8 +164,8 @@ public class Vector {
         final int prime = 13;
         int hash = 1;
 
-        for (int i = 0; i < this.n; i++) {
-            hash = prime * hash + Double.hashCode(this.array[i]);
+        for (double itemArrayDouble : this.arrayDoubles) {
+            hash = prime * hash + Double.hashCode(itemArrayDouble);
         }
         return hash;
     }
