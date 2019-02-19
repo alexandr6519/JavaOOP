02fa41c77443ac7811692681b6ae336041e0d46a
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-02-19 16:05:46 +0700

    Add files via upload
    version of 19.02.19 16:05

--- Vector/src/ru.academits.ikonnikov.vector/vectorMain/MainVector.java
+++ Vector/src/ru.academits.ikonnikov.vector/vectorMain/MainVector.java
@@ -33,23 +33,23 @@ public class MainVector {
         System.out.println(" After changing of values vector v4 : " + v4.toString());
 
         Vector v5 = new Vector(v3);
-        System.out.println("Vector v5 is copy of v3 : " + v5.toString());
+        System.out.printf("Vector v5 is copy of v3 : %s %n ", v5.toString());
 
-        System.out.println("The result of addition of vectors v1 and v2 by nonstatic method is: " + v1.add(v2).toString());
+        System.out.printf("The result of addition of vectors v1 and v2 by nonstatic method is: %s %n", v1.add(v2).toString());
 
-        System.out.println("The result of addition of vectors v1 and v2 by static method is: " + add(v1, v2).toString());
+        System.out.printf("The result of addition of vectors v1 and v2 by static method is: %s %n", add(v1, v2).toString());
 
-        System.out.println("The result of subtraction of vector v2 from v1 by nonstatic method is: " + v1.subtract(v2).toString());
+        System.out.printf("The result of subtraction of vector v2 from v1 by nonstatic method is: %s %n", v1.subtract(v2).toString());
 
-        System.out.println("The result of subtraction of vector v2 from v1 by static method is: " + subtract(v1, v2).toString());
+        System.out.printf("The result of subtraction of vector v2 from v1 by static method is: %s %n", subtract(v1, v2).toString());
 
-        System.out.println("The result of multiplication of vector v2 by scalar (3,30) is: " + v2.multiplyByScalar(3.3).toString());
-        System.out.println("The result of multiplication of vector v2 by scalar (-0,50) is: " + v2.multiplyByScalar(-0.5).toString());
+        System.out.printf("The result of multiplication of vector v2 by scalar (3,30) is: %s %n", v2.multiplyByScalar(3.3).toString());
+        System.out.printf("The result of multiplication of vector v2 by scalar (-0,50) is: %s %n", v2.multiplyByScalar(-0.5).toString());
 
         System.out.printf("The result of scalar multiplication of vectors v2 and v3 = %.2f %n", multiplyScalar(v2, v3));
 
-        System.out.println("The result of comparing vectors v2 and v4 is: " + v4.equals(v2));
-        System.out.println("The result of comparing vectors v3 and v5 is: " + v3.equals(v5));
+        System.out.printf("The result of comparing vectors v2 and v4 is: %s %n", v4.equals(v2));
+        System.out.printf("The result of comparing vectors v3 and v5 is: %s %n", v3.equals(v5));
 
         System.out.printf("  The first component of vector v4 = %.2f %n  The second component of vector v4 = %.2f %n", v4.getComponent(0), v4.getComponent(1));
 
