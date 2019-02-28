937cd7bc2f7f5f5011546513fddfeb54407fb176
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-02-28 11:49:55 +0700

    Add files via upload
    correcting of cod when null

--- ArrayListHome/src/ru.academits.ikonnikov.arrayListHome/ArrayListHome.java
+++ ArrayListHome/src/ru.academits.ikonnikov.arrayListHome/ArrayListHome.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class arrayListHome {
+public class ArrayListHome {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(new FileInputStream("./res/input.txt"));
@@ -21,19 +21,16 @@ public class arrayListHome {
                 }
                 System.out.println(list);
             }
-        } catch ( FileNotFoundException e ) {
-            e.getMessage();
+        } catch (FileNotFoundException e) {
+            System.out.println("The file is not exist!");
+            e.printStackTrace();
         }
 
-        ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(13, 4, 18, 9, null, 10, 9, 14, 19, 2, 4, 16, 1, 7, 27, 13, 6, 11));
+        ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(13, 4, 18, 9, 10, 9, 14, 19, 2, 4, 16, 1, 7, 27, 13, 6, 11));
 
         System.out.println(numbers1);
 
         for (int i = 0; i < numbers1.size(); i++) {
-            if (numbers1.get(i) == null) {
-                continue;
-            }
-
             if (numbers1.get(i) % 2 == 0) {
                 numbers1.remove(i);
                 i--;
@@ -41,37 +38,17 @@ public class arrayListHome {
         }
         System.out.println(numbers1);
 
-        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(null, 7, 2, 19, 12, null, 4, 5, 12, 17, 6, 14, 12, 19, 17, 7, 3, 4, 14));
+        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(7, 2, 19, 12, 4, 5, null, 12, 17, 6, 14, 12, 19, 17, 7, 3, 4, 14));
+        ArrayList<Integer> numbersNoRepeats = new ArrayList<>();
+        System.out.println(numbers2);
 
-        if (numbers2.isEmpty()) {
-            System.out.println("This ArrayList is empty!");
-        } else {
-            System.out.println(numbers2);
-            ArrayList<Integer> numbersNoRepeats = new ArrayList<>();
-            boolean existNull = false;
-
-            if (numbers2.get(0) == null) {
-                numbersNoRepeats.add(null);
-                existNull = true;
-            } else {
-                numbersNoRepeats.add(numbers2.get(0));
-            }
-
-            for (int i = 1; i < numbers2.size(); i++) {
-                if (numbers2.get(i) == null) {
-                    if (existNull) {
-                        continue;
-                    } else {
-                        numbersNoRepeats.add(null);
-                        existNull = true;
-                        continue;
-                    }
-                }
-                if (numbersNoRepeats.indexOf(numbers2.get(i)) == -1) {
-                    numbersNoRepeats.add(numbers2.get(i));
+        if (!numbers2.isEmpty()) {
+            for (Integer integer : numbers2) {
+                if (!numbersNoRepeats.contains(integer)) {
+                    numbersNoRepeats.add(integer);
                 }
             }
-            System.out.println(numbersNoRepeats);
         }
+        System.out.println(numbersNoRepeats);
     }
 }
