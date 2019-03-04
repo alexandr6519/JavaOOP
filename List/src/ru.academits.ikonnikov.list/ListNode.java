3398134bdc978c22d0181a38c857020f581dd1b9
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-03-04 14:58:07 +0700

    Add files via upload
    the moving of file to another directory

---
+++ List/src/ru.academits.ikonnikov.list/ListNode.java
@@ -0,0 +1,44 @@
+package ru.academits.ikonnikov.list.node;
+
+public class ListNode<T> {
+    private T data;
+    private ListNode<T> next;
+
+    public ListNode(T data) {
+        this.data = data;
+    }
+
+    public ListNode(T data, ListNode<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public ListNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getData() == null) {
+            return "Node is null!";
+        }
+        if (next == null) {
+            return "Node :[DATA: " + data + ", NEXT: NULL" + "]";
+        }
+        return "Node :[DATA: " + data + ", NEXT: " + next.getData() + "]";
+    }
+}
+
+
