705e548d9a1d745706c68369cbfdcf8c3b7ae32d
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-03-08 23:29:43 +0700

    Add files via upload
    correecting of cod 08.03.19

--- List/src/ru.academits.ikonnikov.list/main/ListMain.java
+++ List/src/ru.academits.ikonnikov.list/main/ListMain.java
@@ -1,47 +1,43 @@
 package ru.academits.ikonnikov.list.main;
 
 import ru.academits.ikonnikov.list.classes.List;
-import ru.academits.ikonnikov.list.node.ListNode;
 
 public class ListMain {
     public static void main(String[] args) {
-        ListNode<String> head = new ListNode<>("Yes");
-
-        List<String> list = new List<>(head, 1);
+        List<String> list = new List<>("Yes", 1);
 
         list.insertInHead("Good buy");
         list.insertByIndex(1, "Hello");
         list.insertByIndex(2, "Thank you");
-        list.insertByIndex(3, "Please");
-        System.out.println("The list is:" + list.toString());
+        list.insertByIndex(3,"Please");
+       System.out.println("The list is: " + list.toString());
 
         System.out.println("The size of this list is: " + list.getLength());
 
-        System.out.println("The head of list is: " + list.getHead().toString());
-
         System.out.println("The data of head is: " + list.getHeadData());
-        System.out.println("The data by index '4' is : " + list.getData(4));
+
+        System.out.println("The data by index '1' is : " + list.getData(1));
         System.out.println("The data by index '4' before setting is : " + list.setData(4, "Not"));
-        System.out.println("The list after changing data by index '4' is :" + list.toString());
+        System.out.println("The list after changing data by index '4' is : " + list.toString());
 
-        System.out.println("The data by index '2' is: " + list.getData(2));
+        List<String> list1 = list.copy();
+        System.out.println("The copy of list is: " + list1.toString());
 
-        System.out.println("The copy of list is:" + list.copy().toString());
-
-        System.out.println("The list after turning is :" + list.turn().toString());
+        list.turn();
+        System.out.println("The list after turning is : " + list.toString());
 
         System.out.println("The result of removing by data 'Well' is: " + list.removeNodeByValue("Well"));
         System.out.println("The list after removing by data 'Well' is :" + list.toString());
 
         System.out.println("The result of removing by data 'Hello' is: " + list.removeNodeByValue("Hello"));
-        System.out.println("The list after removing by data 'Hello' is :" + list.toString());
+        System.out.println("The list after removing by data 'Hello' is : " + list.toString());
 
         System.out.println("The removing data by index '2' is: " + list.removeNode(2));
-        System.out.println("The list after removing data by index '2' is :" + list.toString());
+        System.out.println("The list after removing data by index '2' is : " + list.toString());
 
-        System.out.println("The data of removing of 'head' is: " + list.removeHead());
+        System.out.println("The data of removing 'head' is: " + list.removeHead());
         System.out.println("The list after removing 'head' is : " + list.toString());
 
-        System.out.println("The head of list is: " + list.getHead().toString());
+        System.out.println("The copy of list is: " + list.copy().toString());
     }
 }
