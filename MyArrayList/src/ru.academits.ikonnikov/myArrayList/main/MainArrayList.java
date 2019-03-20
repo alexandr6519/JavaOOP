ee2947b61067a4801fde6284c498cbc8c4abda2a
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-03-20 15:51:37 +0700

    Add files via upload
    correcting of cod by remarkes of 18.03.19

--- MyArrayList/src/ru.academits.ikonnikov/myArrayList/main/MainArrayList.java
+++ MyArrayList/src/ru.academits.ikonnikov/myArrayList/main/MainArrayList.java
@@ -1,4 +1,4 @@
-package ru.acadeits.ikonnikov.myArrayList.main;
+package ru.academits.ikonnikov.myArrayList.main;
 
 import ru.academits.ikonnikov.myArrayList.classes.MyArrayList;
 
@@ -9,92 +9,130 @@ import java.util.List;
 
 public class MainArrayList {
     public static void main(String[] args) {
-        String[] arrayStrings = {"a", "b", "null", "d", "e"};
-        MyArrayList<String> myListStrings = new MyArrayList<>(arrayStrings);
-        System.out.println("The myListStrings  is : " + myListStrings.toString());
+        MyArrayList<String> myListStringsEmpty = new MyArrayList<>(0);
+        System.out.println("The myListStringsEmpty is: " + myListStringsEmpty.toString());
+        System.out.println("The length of myListStringsEmpty.toArray()is: " + myListStringsEmpty.toArray().length);
+        myListStringsEmpty.clear();
+        System.out.println("The myListStringsEmpty after clearing is: " + myListStringsEmpty.toString());
+        System.out.println();
 
         Integer[] arrayIntegers = {0, 1, 2, null, 4, 5, 6};
         MyArrayList<Integer> myListIntegers = new MyArrayList<>(arrayIntegers);
 
-        Iterator iterator = myListIntegers.iterator();
+        Iterator iteratorInt = myListIntegers.iterator();
         System.out.print("The items of myListIntegers:   ");
 
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next() + "   ");
+        while (iteratorInt.hasNext()) {
+            System.out.print(iteratorInt.next() + "   ");
         }
         System.out.println();
 
-        System.out.println("The result of adding data '7' in myListIntegers is : " + myListIntegers.add(7));
+        System.out.println("The result of adding data '7' to myListIntegers is : " + myListIntegers.add(7));
         System.out.println("The myListIntegers after adding data '7' is : " + myListIntegers.toString());
 
-        System.out.println("The pastData of setting data '7' in myListIntegers by index '1' is: " + myListIntegers.set(1, 7));
+        System.out.println("The pastData of setting data '7' to myListIntegers by index '1' is: " + myListIntegers.set(1, 7));
         System.out.println("The myListIntegers after setting data '7' by index '1' is : " + myListIntegers.toString());
-        System.out.println("The result of getting by index '2' in myListIntegers is: " + myListIntegers.get(2));
+        System.out.println("The item of myListIntegers by index '1' is: " + myListIntegers.get(1));
 
-        System.out.println("The pastData of setting data 'k' in myListStrings  by index '1' is: " + myListStrings.set(1, "k"));
-        System.out.println("The result of getting by index '2' in myListStrings is: " + myListStrings.get(2));
+        System.out.println("The result of method 'indexOf(1)' of myListIntegers is: " + myListIntegers.indexOf(1));
+        System.out.println("The result of method 'indexOf(null)' of myListIntegers is: " + myListIntegers.indexOf(null));
+        System.out.println("The result of method 'indexOf(7)' of myListIntegers is: " + myListIntegers.indexOf(7));
+        System.out.println("The result of method 'lastIndexOf(7)' of myListIntegers is: " + myListIntegers.lastIndexOf(7));
+        System.out.println("The result of method 'lastIndexOf(1)' of myListIntegers is: " + myListIntegers.lastIndexOf(1));
+        System.out.println("The result of method 'lastIndexOf(0)' of myListIntegers is: " + myListIntegers.lastIndexOf(0));
 
-        System.out.println("The result of adding data 'f' in myListStrings  is : " + myListStrings.add("f"));
+        Integer[] arrayForIntegers = new Integer[0];
+        Integer[] arrayListToArrayInt = myListIntegers.toArray(arrayForIntegers);
+
+        System.out.print("The array of method 'myListIntegers.toArray(arrayForIntegers)' is : (");
+
+        for (int i = 0; i < arrayListToArrayInt.length - 1; i++) {
+            System.out.print(arrayListToArrayInt[i] + ", ");
+        }
+        System.out.println(arrayListToArrayInt[arrayListToArrayInt.length - 1] + ")");
+        System.out.println();
+
+        String[] arrayStrings = {"a", "b", "null", "d", "e"};
+        MyArrayList<String> myListStrings = new MyArrayList<>(arrayStrings);
+        System.out.println("The myListStrings  is : " + myListStrings.toString());
+        System.out.println("The pastData of setting data 'k' to myListStrings  by index '1' is: " + myListStrings.set(1, "k"));
+        System.out.println("The item of myListStrings by index '2' in  is: " + myListStrings.get(2));
+
+        System.out.println("The result of adding data 'f' to myListStrings  is : " + myListStrings.add("f"));
         System.out.println("The myListStrings  after adding data 'f' is : " + myListStrings.toString());
 
-        List<String> collection1 = new ArrayList<>(Arrays.asList("s", "q"));
-        System.out.println("The result of adding collection {'s','q'} in myListStrings is : " + myListStrings.addAll(collection1));
-        System.out.println("The myListStrings  after adding collection {'s','q'} is : " + myListStrings.toString());
+        List<String> collection1 = new ArrayList<>(Arrays.asList("s", "q", "p"));
+        System.out.println("The result of adding collection {'s','q','p'} to myListStrings is : " + myListStrings.addAll(collection1));
+        System.out.println("The myListStrings  after adding collection {'s','q','p'} is : " + myListStrings.toString());
 
         List<String> collection2 = new ArrayList<>(Arrays.asList("4", "8"));
-        System.out.println("The result of adding collection {'4','8'} by index '3' in myListStrings  is : " + myListStrings.addAll(3, collection2));
+
+        System.out.println("The result of adding collection {'4','8'} to myListStrings by index '3' is : " + myListStrings.addAll(3, collection2));
         System.out.println("The myListStrings after adding collection {'4','8'} is : " + myListStrings.toString());
 
-        System.out.println("The 'indexOf' item  '1' in myListIntegers is: " + myListIntegers.indexOf(1));
-        System.out.println("The 'lastIndexOf' item '7' in myListIntegers is: " + myListIntegers.lastIndexOf(7));
         System.out.println("The removed data from myListStrings  by index '1' is: " + myListStrings.remove(1));
-        System.out.println("The result of removing from myListStrings  object 'f' is: " + myListStrings.remove("f"));
-        System.out.println("The myList1 after removing from myListStrings  object 'f' is : " + myListStrings.toString());
+        System.out.println("The result of removing of object 'f' from myListStrings   is: " + myListStrings.remove("f"));
+        System.out.println("The myListStrings after removing of object 'f' and data by index '1' is : " + myListStrings.toString());
+
+        System.out.println("The result of removing collection {'4','8'} from myListStrings  is : " + myListStrings.removeAll(collection2));
+        System.out.println("The myListStrings  after removing collection {'4','8'} is : " + myListStrings.toString());
+
+        Object[] arrayListToArrayStr = myListStrings.toArray();
+        System.out.print("The array by method 'myListStrings.toArray()' is : (");
+
+        for (int i = 0; i < arrayListToArrayStr.length - 1; i++) {
+                System.out.print(arrayListToArrayStr[i] + ", ");
+        }
+        System.out.println(arrayListToArrayStr[arrayListToArrayStr.length - 1] + ")");
+
+        System.out.println("The result of adding data 'q' to myListStrings  is :  " + myListStrings.add("q"));
+        System.out.println("The myListStrings  is : " + myListStrings.toString());
+
+        if (myListStrings.containsAll(collection1)) {
+            System.out.println("The myListStrings has collection {'s','q','p'}.");
+        } else {
+            System.out.println("The myListStrings has not collection {'s','q','p'}.");
+        }
+
+        if (myListStrings.containsAll(collection2)) {
+            System.out.println("The myListStrings has collection {'4','8'}." );
+        } else {
+            System.out.println("The myListStrings has not collection {'4','8'}." );
+        }
+        System.out.println("The result of using method 'retainAll' to myListStrings by collection {'s','q','p'} is : " + myListStrings.retainAll(collection1));
+        System.out.println("The myListStrings after using method 'retainAll' by collection {'s','q','p'} is : " + myListStrings.toString());
+        System.out.println();
 
         MyArrayList<String> myListStringsCopy = new MyArrayList<>(arrayStrings);
         System.out.println("The myListStringCopy is : " + myListStringsCopy.toString());
-
-        System.out.println("The result of removing collection {'s','q'} from myListStrings  is : " + myListStrings.removeAll(collection1));
-        System.out.println("The myListStrings  after removing collection {'s','q'} is : " + myListStrings.toString());
-
         myListStringsCopy.clear();
         System.out.println("The myListStringsCopy result after clearing is : " + myListStringsCopy.toString());
         System.out.println("The result of checking on emptiness of myListStringsCopy  is : " + myListStringsCopy.isEmpty());
 
-        MyArrayList<Short> myListShorts = new MyArrayList<>(5);
+        List<Short> collection3 = new ArrayList<>(Arrays.asList((short)14, (short)16, (short)18));
+        MyArrayList<Short> myListShorts = new MyArrayList<>(6);
         System.out.println("The size of myListShorts = " + myListShorts.size());
         myListShorts.add(0, (short) 11);
         myListShorts.add(1, (short) 22);
         myListShorts.add((short) 33);
-        System.out.println("The myListShorts is : " + myListShorts.toString());
-        System.out.println("The result of getting by index '5' in myListShorts is: " + myListShorts.get(5));
+        myListShorts.addAll(3, collection3);
+        System.out.println("The myListShorts after adding items and collection is : " + myListShorts.toString());
+        System.out.println("The item of myListShorts by index '5' is: " + myListShorts.get(5));
 
-        System.out.println("The result of checking of presence of item '11' in myListShorts by method 'contains' is : " + myListShorts.contains((short) 11));
-
-        Object[] arrayListToArrayObject = myListIntegers.toArray();
-        System.out.print("The result of method 'myListIntegers.toArray()' is : {");
-
-        for (int i = 0; i < arrayListToArrayObject.length - 1; i++) {
-                System.out.print(arrayListToArrayObject[i] + ", ");
+        if (myListShorts.contains((short) 12)) {
+            System.out.println("The myListShorts has item '12'.");
+        } else {
+            System.out.println("The myListShorts has not item '12'.");
         }
-        System.out.println(arrayListToArrayObject[arrayListToArrayObject.length - 1] + "}");
 
-        myListStrings.add(2, "q");
-        System.out.println("The myListStrings  is : " + myListStrings.toString());
-        System.out.println("The result of checking of presence of collection {'s','q'} in myListStrings is : " + myListStrings.containsAll(collection1));
-        System.out.println("The result of checking of presence of collection {'4','8'} in myListStrings is : " + myListStrings.containsAll(collection2));
-
-        System.out.println("The result of using method 'retainAll' by collection {'s','q'} from myListStrings  is : " + myListStrings.retainAll(collection1));
-        System.out.println("The myListStrings using method 'retainAll' by collection {'s','q'} is : " + myListStrings.toString());
-
-        Integer[] arrayForIntegers = new Integer[myListIntegers.size()];
-        Integer[] arrayListToArray = myListIntegers.toArray(arrayForIntegers);
-
-        System.out.print("The result of method 'myListIntegers.toArray(arrayForIntegers)' is : {");
-
-        for (int i = 0; i < arrayListToArray.length - 1; i++) {
-                System.out.print(arrayListToArray[i] + ", ");
+        if (myListShorts.contains((short) 11)) {
+            System.out.println("The myListShorts has item '11'.");
+        } else {
+            System.out.println("The myListShorts has not item '11'.");
         }
-        System.out.println(arrayListToArray[arrayListToArray.length - 1] + "}");
+
+        if (myListShorts.retainAll(collection3)) {
+            System.out.println("The myListShorts after using method 'retainAll' by collection {'14','16','18'} is : " + myListShorts.toString());
+        }
     }
 }
