287cfe12acc64b6f9b7041a4529a4cea3a594934
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-04-10 15:26:16 +0700

    Add files via upload
    the correcting of cod by remarks of 09.04.19

--- HashTable/src/ru.academits.ikonnikov.hashTable/classes/MyHashTable.java
+++ HashTable/src/ru.academits.ikonnikov.hashTable/classes/MyHashTable.java
@@ -29,9 +29,6 @@ public class MyHashTable<T> implements Collection<T> {
     }
 
     private int calculateObjectIndex(Object object) {
-        if (lists.length == 0) {
-            throw new IllegalArgumentException("This hashTable is null!");
-        }
         if (object == null) {
             return 0;
         }
@@ -45,12 +42,7 @@ public class MyHashTable<T> implements Collection<T> {
         if (lists[indexObject] == null) {
             return false;
         }
-        for (T item : lists[indexObject]) {
-            if (Objects.equals(item, object)) {
-                return true;
-            }
-        }
-        return false;
+        return lists[indexObject].contains(object);
     }
 
     @Override
@@ -67,6 +59,7 @@ public class MyHashTable<T> implements Collection<T> {
     }
 
     @Override
+
     public boolean add(T t) {
         int indexObject = calculateObjectIndex(t);
 
@@ -85,14 +78,12 @@ public class MyHashTable<T> implements Collection<T> {
         if (c == null) {
             throw new IllegalArgumentException("This collection is null!");
         }
-        boolean wasAddAll = false;
 
-        for (Object item : c) {
-                //noinspection unchecked
-                this.add((T) item);
-                wasAddAll = true;
+        for (T item : c) {
+            this.add(item);
         }
-        return wasAddAll;
+
+        return c.size() > 0;
     }
 
     @Override
@@ -102,19 +93,10 @@ public class MyHashTable<T> implements Collection<T> {
         if (lists[indexObject] == null) {
             return false;
         }
-        for (T item : lists[indexObject]) {
-            if (Objects.equals(item, object)) {
-                if (lists[indexObject].size() == 1) {
-                    lists[indexObject] = null;
-                } else {
-                    lists[indexObject].remove(item);
-                }
-                modCount++;
-                size--;
-                return true;
-            }
-        }
-        return false;
+
+        modCount++;
+        size--;
+        return lists[indexObject].remove(object);
     }
 
     @Override
@@ -126,12 +108,7 @@ public class MyHashTable<T> implements Collection<T> {
 
         for (ArrayList<T> list : lists) {
             if (list != null) {
-                for (T item : list) {
-                    if (c.contains(item)) {
-                        remove(item);
-                        wasRemovedAll = true;
-                    }
-                }
+                wasRemovedAll = list.removeAll(c) || wasRemovedAll;
             }
         }
         return wasRemovedAll;
@@ -146,13 +123,7 @@ public class MyHashTable<T> implements Collection<T> {
 
         for (ArrayList<T> list : lists) {
             if (list != null) {
-                for (int j = 0; j < list.size(); j++) {
-                    if (!c.contains(list.get(j))) {
-                        list.remove(j);
-                        j--;
-                        wasRetainedAll = true;
-                    }
-                }
+                wasRetainedAll = list.retainAll(c) || wasRetainedAll;
             }
         }
         return wasRetainedAll;
@@ -171,9 +142,101 @@ public class MyHashTable<T> implements Collection<T> {
         size = 0;
     }
 
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyTableIterator();
+    }
+
+    private int getIndexNextNotNullList(int start) {
+        if (start < 0 || start >= lists.length) {
+            throw new IllegalArgumentException("The value of argument isn't correct!");
+        }
+
+        for (int i = start; i < lists.length; i++) {
+            if (lists[i] != null && lists[i].size() > 0) {
+                return i;
+            }
+        }
+        return lists.length - 1;
+    }
+
+    private class MyTableIterator implements Iterator<T> {
+        private int currentIndexArray = getIndexNextNotNullList(0);
+        private int currentIndexList = -1;
+        private int modCountCurrent = modCount;
+
+        @Override
+        public boolean hasNext() {
+            if (currentIndexArray + 1 < lists.length) {
+                return true;
+            }
+            if (lists[lists.length - 1] == null) {
+                return false;
+            }
+            return currentIndexList + 1 < lists[lists.length - 1].size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("The next element is absent!");
+            }
+            if (modCountCurrent != modCount) {
+                throw new ConcurrentModificationException("There was adding or removing elements!");
+            }
+            T item = lists[currentIndexArray].get(++currentIndexList);
+
+            if (currentIndexList + 1 == lists[currentIndexArray].size() && currentIndexArray++ + 1 < lists.length) {
+                currentIndexList = -1;
+                currentIndexArray = getIndexNextNotNullList(currentIndexArray);
+            }
+            return item;
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] arrayForLists = new Object[size];
+        int i = 0;
+
+        for (T item : this) {
+            arrayForLists[i] = item;
+
+            if (i < size - 1) {
+                i++;
+            }
+        }
+
+        return arrayForLists;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("This array is null!");
+        }
+        if (array.length < size) {
+            //noinspection unchecked
+            array = (T1[]) Arrays.copyOf(array, size, array.getClass());
+        }
+        int i = 0;
+
+        for (T item : this) {
+            //noinspection unchecked
+            array[i] = (T1) item;
+            i++;
+        }
+
+        if (array.length > size) {
+            array[size] = null;
+        }
+        return array;
+    }
+
     @Override
     public String toString() {
-        if (size == 0 || lists == null) {
+        if (lists.length == 0) {
             return "{ }";
         }
         StringBuilder result = new StringBuilder("{").append(System.lineSeparator());
@@ -197,100 +260,5 @@ public class MyHashTable<T> implements Collection<T> {
             }
         }
         return result.append("}").toString();
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new MyTableIterator();
-    }
-
-    private class MyTableIterator implements Iterator<T> {
-        private int currentIndexArray = 0;
-        private int currentIndexList = 0;
-        private int modCountCurrent = modCount;
-        private boolean needContinue = true;
-
-        @Override
-        public boolean hasNext() {
-            if (!needContinue) {
-                return false;
-            }
-            if (lists[lists.length - 1] == null) {
-                return currentIndexArray + 1 < lists.length;
-            }
-            return (currentIndexArray + 1 < lists.length || currentIndexList < lists[lists.length - 1].size());
-        }
-
-        @Override
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException("The next element is absent!");
-            }
-            if (modCountCurrent != modCount) {
-                throw new ConcurrentModificationException("There was adding or removing elements!");
-            }
-            while (currentIndexArray + 1 < lists.length && lists[currentIndexArray] == null) {
-                currentIndexArray++;
-            }
-            T item = lists[currentIndexArray].get(currentIndexList);
-
-            if (currentIndexList == lists[currentIndexArray].size() - 1) {
-                currentIndexList = 0;
-
-                if (currentIndexArray < lists.length - 1) {
-                    currentIndexArray++;
-                } else {
-                    needContinue = false;
-                }
-            } else {
-                currentIndexList++;
-            }
-            return item;
-        }
-    }
-
-    @Override
-    public Object[] toArray() {
-        Object[] arrayForLists = new Object[size];
-        int i = 0;
-
-        for (ArrayList list : this.lists) {
-            if (list != null) {
-                for (Object item : list) {
-                    arrayForLists[i] = item;
-
-                    if (i < size - 1) {
-                        i++;
-                    }
-                }
-            }
-        }
-        return arrayForLists;
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] array) {
-        if (array == null) {
-            throw new IllegalArgumentException("This array is null!");
-        }
-        if (array.length < size) {
-            //noinspection unchecked
-            array = (T1[]) Arrays.copyOf(array, size, array.getClass());
-        }
-        int i = 0;
-
-        for (ArrayList list : this.lists) {
-            if (list != null) {
-                for (Object item : list) {
-                    //noinspection unchecked
-                    array[i] = (T1) item;
-                    i++;
-                }
-            }
-        }
-        if (array.length > size) {
-            array[size] = null;
-        }
-        return array;
     }
 }
