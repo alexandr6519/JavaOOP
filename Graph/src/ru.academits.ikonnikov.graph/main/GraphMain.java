70ea6059f85c1f38fa4989c931a5858969e8a2de
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-04-24 15:33:27 +0700

    Update GraphMain.java

--- Graph/src/ru.academits.ikonnikov.graph/main/GraphMain.java
+++ Graph/src/ru.academits.ikonnikov.graph/main/GraphMain.java
@@ -5,26 +5,47 @@ import ru.academits.ikonnikov.graph.MyGraph;
 import java.util.LinkedList;
 
 public class GraphMain {
-    public static void main(String[] args) {
-        MyGraph myGraph = new MyGraph(11);
+    String [] arrayCities = new String[] {"Novosibirsk", "Kemerovo", "Omsk", "Krasnoyarsk", "Tomsk", "Moscow", "St.Peterburg", "Abakan",
+        "Irkutsk", "Ufa", "Kazan"};
 
-        if (myGraph.addVertex()) {
-            System.out.printf("The item [%2s] was added to myGraph.%n", myGraph.getVertexCount() - 1);
+        LinkedList<String> listCities = new LinkedList<>(Arrays.asList(arrayCities));
+        listCities.add("Yakutsk");
+        MyGraph myGraph = new MyGraph(listCities.size());
+
+        int index1 = listCities.indexOf("Novosibirsk");
+        int index2 = listCities.indexOf("Kemerovo");
+
+        if (myGraph.addEdge(index1, index2)) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", arrayCities[index1], arrayCities[index2]);
         }
-
-        if (myGraph.addEdge(0, 1)) {
-            System.out.printf("The edge (%s , %s) was added.%n",0, 1);
+        if (myGraph.addEdge(listCities.indexOf("Novosibirsk"), listCities.indexOf("Moscow"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Novosibirsk", "Moscow");
         }
-
-        myGraph.addEdge(0, 5);
-        myGraph.addEdge(0, 6);
-        myGraph.addEdge(5, 1);
-        myGraph.addEdge(2, 5);
-        myGraph.addEdge(4, 5);
-        myGraph.addEdge(3, 7);
-        myGraph.addEdge(3, 8);
-        myGraph.addEdge(2, 4);
-        myGraph.addEdge(9, 10);
+        if (myGraph.addEdge(listCities.indexOf("Novosibirsk"), listCities.indexOf("St.Peterburg"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Novosibirsk", "St.Peterburg");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Kemerovo"), listCities.indexOf("Moscow"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Kemerovo", "Moscow");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Omsk"), listCities.indexOf("Moscow"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Omsk", "Moscow");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Tomsk"), listCities.indexOf("Moscow"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Tomsk", "Moscow");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Krasnoyarsk"), listCities.indexOf("Abakan"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Krasnoyarsk", "Abakan");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Krasnoyarsk"), listCities.indexOf("Irkutsk"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Krasnoyarsk", "Irkutsk");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Omsk"), listCities.indexOf("Tomsk"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Omsk", "Tomsk");
+        }
+        if (myGraph.addEdge(listCities.indexOf("Kazan"), listCities.indexOf("Ufa"))) {
+            System.out.printf("The edge (%s , %s) was added to myGraph.%n", "Kazan", "Ufa");
+        }
+        System.out.println();
 
         int[][] matrix = myGraph.getEdges();
         System.out.println("The matrix of edges is :");
@@ -39,11 +60,11 @@ public class GraphMain {
 
         System.out.println("The result of method (goAroundInDepth) is : ");
         myGraph.goAroundInDepth((item) -> {
-            System.out.printf("The vertex is: [%s], verticesConnected are : { ", item);
+            System.out.printf("The vertex is: [%12s], verticesConnected are : { ", listCities.get(item));
             LinkedList<Integer> children = myGraph.getChildren(item);
 
-            if (children != null) {
-                System.out.printf("%s ", children.toString());
+            for (Integer indexItem : children) {
+                System.out.printf("[%s] ", listCities.get(indexItem));
             }
             System.out.println("}");
         });
@@ -51,11 +72,11 @@ public class GraphMain {
 
         System.out.println("The result of method (goAroundInWidth) is : ");
         myGraph.goAroundInWidth((item) -> {
-            System.out.printf("The vertex is: [%s], verticesConnected are : { ", item);
+            System.out.printf("The vertex is: [%12s], verticesConnected are : { ", listCities.get(item));
             LinkedList<Integer> children = myGraph.getChildren(item);
 
-            if (children != null) {
-                System.out.printf("%s ", children.toString());
+            for (Integer indexItem : children) {
+                System.out.printf("[%s] ", listCities.get(indexItem));
             }
             System.out.println("}");
         });
