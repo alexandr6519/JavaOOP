0e56b56cb39f06e25ca8721acc5ce96cc8e39904
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-04-28 10:28:26 +0700

    Add files via upload
    the correcting of cod by remarks of 27.04.19

--- Lambda/src/ru.academits.ikonnikov.lambda/main/LambdaMain.java
+++ Lambda/src/ru.academits.ikonnikov.lambda/main/LambdaMain.java
@@ -22,31 +22,46 @@ public class LambdaMain {
         personsList.add(new Person("Elena", 21));
         personsList.add(new Person("Elena", 39));
 
-        Stream<Person> personStream = personsList.stream();
-
-        List<String> namesList = personStream.map(Person::getName).sorted().collect(Collectors.toList());
+        List<String> namesList = personsList.stream().map(Person::getName)
+                .sorted()
+                .collect(Collectors.toList());
         System.out.println("The sorted list of names is:  " + namesList);
 
-        String personNamesUnique = personsList.stream().map(Person::getName).distinct().collect(Collectors.joining(", "));
+        String personNamesUnique = personsList.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.joining(", "));
         System.out.println("The unique names: " + personNamesUnique);
         System.out.println();
 
-        List<Person> teensList = personsList.stream().filter(x -> x.getAge() < 18).collect(Collectors.toList());
+        List<Person> teensList = personsList.stream()
+                .filter(x -> x.getAge() < 18)
+                .collect(Collectors.toList());
         System.out.printf("The list of teens is : %n %s%n", teensList);
 
-        OptionalDouble ageAverage = personsList.stream().mapToDouble(Person::getAge).filter(x -> x < 18).average();
-        //noinspection OptionalGetWithoutIsPresent
-        System.out.printf("The average of ages of teens is : %.2f %n", ageAverage.getAsDouble());
-        System.out.println();
+        if (teensList.size() > 0) {
+            OptionalDouble ageAverage = personsList.stream()
+                    .mapToDouble(Person::getAge)
+                    .filter(x -> x < 18)
+                    .average();
+            //noinspection OptionalGetWithoutIsPresent
+            System.out.printf("The average of ages of teens is : %.2f %n", ageAverage.getAsDouble());
+            System.out.println();
+        } else {
+            System.out.println("The personList has not person with age less than 18.");
+        }
 
-        Map<String, Double> map = personsList.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
+        Map<String, Double> map = personsList.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
         System.out.println("The map as result of grouping by name is :");
         map.forEach((name, age) -> System.out.printf("The KEY (as name) is : %10s, the VALUE (as average age) is : %.2f%n", name, age));
         System.out.println();
 
-        Stream<Person> streamPersonMiddleAge = personsList.stream().filter((x) -> x.getAge() > 19 && x.getAge() < 46);
-        List<Person> listPersonMiddleAge = streamPersonMiddleAge.sorted((p1, p2) -> p2.getAge() - p1.getAge()).collect(Collectors.toList());
+        List<Person> listPersonMiddleAge = personsList.stream()
+                .filter(x -> x.getAge() >= 20 && x.getAge() <= 45)
+                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
+                .collect(Collectors.toList());
         System.out.println("The persons of middle age (from 20 to 45) are : ");
-        listPersonMiddleAge.forEach((name) -> System.out.printf("%s %n", name));
+        listPersonMiddleAge.forEach(name -> System.out.printf("%s %n", name));
     }
 }
