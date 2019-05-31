e37d14985c52487492950605bf50d247397d809d
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-05-31 22:08:54 +0700

    Update Temperature.java

--- Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
@@ -2,16 +2,7 @@ package ru.academits.ikonnikov.temperature;
 
 import javax.swing.*;
 
-public class Temperature {
-    /*private static boolean isNumeric(String stringNumber) {
-        try {
-            Double.parseDouble(stringNumber);
-            return true;
-        } catch ( NumberFormatException e ) {
-            return false;
-        }
-    }*/
-
+public class Temperature {  
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(
