4cc3bd36f95418211cbda85872d921ffcbf8ae05
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-06 00:03:34 +0700

    Add files via upload
    the correcting of cod by remarks of 03.06.19

--- Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
@@ -2,20 +2,10 @@ package ru.academits.ikonnikov.temperature;
 
 import ru.academits.ikonnikov.temperature.gui.View;
 
-import javax.swing.*;
-
 public class Temperature {
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
-
-        SwingUtilities.invokeLater(() -> {
-            View view = new View();
-            view.run();
-            view.setVisible(true);
-        });
+        View view = new View();
+        view.run();
+        view.setVisible(true);
     }
 }
