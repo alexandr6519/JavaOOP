4d3a72da2a21d0275171fd5c3a198e4419a8d064
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-09 21:52:51 +0700

    Add files via upload
    correcting of cod by remarks of  09.06.19

--- Temperature/src/ru.academits.ikonnikov.temperature/gui/Scales.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/gui/Scales.java
@@ -1,30 +1,22 @@
 package ru.academits.ikonnikov.temperature.gui;
 
 class Scales {
-    private int scaleInitial;
-    private int scaleToConvert;
     private String[] scales;
 
     Scales() {
         this.scales = new String[]{"Celsius", "Fahrenheit", "Kelvin"};
     }
 
+    int getScaleIndex(String scale) {
+        for (int i = 0; i < scales.length; i++) {
+            if (scale.equals(scales[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     String[] getScales() {
         return scales;
-    }
-    int getScaleInitial() {
-        return scaleInitial;
-    }
-
-    void setScaleInitial(int scaleInitial) {
-        this.scaleInitial = scaleInitial;
-    }
-
-    int getScaleToConvert() {
-        return scaleToConvert;
-    }
-
-    void setScaleToConvert(int scaleToConvert) {
-        this.scaleToConvert = scaleToConvert;
     }
 }
