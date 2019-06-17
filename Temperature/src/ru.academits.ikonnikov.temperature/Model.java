afed4d582f087995d49710022395c579ca182253
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-17 22:37:12 +0700

    Add files via upload
    the correcting of cod by remarks of 13.06.19

--- Temperature/src/ru.academits.ikonnikov.temperature/Model.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Model.java
@@ -1,7 +1,14 @@
 package ru.academits.ikonnikov.temperature;
 
+import ru.academits.ikonnikov.temperature.gui.Scales;
+
 public class Model {
     private double outputTemperature;
+    private Scales scales;
+
+    public Model(Scales scales) {
+        this.scales = scales;
+    }
 
     public double getOutputTemperature() {
         return outputTemperature;
@@ -12,60 +19,11 @@ public class Model {
     }
 
     public boolean wasConvertTemperature(double inputTemperature, int scaleInitial, int scaleToConvert) {
-        switch (scaleInitial) {
-            case 0:
-                if (inputTemperature < -273.15) {
-                    return false;
-                }
-
-                switch (scaleToConvert) {
-                    case 0:
-                        setOutputTemperature(inputTemperature);
-                        break;
-                    case 1:
-                        setOutputTemperature(inputTemperature * 9 / 5 + 32);
-                        break;
-                    case 2:
-                        setOutputTemperature(inputTemperature + 273.15);
-                        break;
-                }
-                return true;
-            case 1:
-                if (inputTemperature < -459.67) {
-                    return false;
-                }
-
-                switch (scaleToConvert) {
-                    case 0:
-                        setOutputTemperature((inputTemperature - 32) * 5 / 9);
-                        break;
-                    case 1:
-                        setOutputTemperature(inputTemperature);
-                        break;
-                    case 2:
-                        setOutputTemperature((inputTemperature - 32) * 5 / 9 + 273.15);
-                        break;
-                }
-                return true;
-            case 2:
-                if (inputTemperature < 0) {
-                    return false;
-                }
-
-                switch (scaleToConvert) {
-                    case 0:
-                        setOutputTemperature(inputTemperature - 273.15);
-                        break;
-                    case 1:
-                        setOutputTemperature((inputTemperature - 273.15) * 9 / 5 + 32);
-                        break;
-                    case 2:
-                        setOutputTemperature(inputTemperature);
-                        break;
-                }
-                return true;
-            default:
-                return false;
+        if (scales.isConvertTemperature(inputTemperature, scaleInitial, scaleToConvert)) {
+            setOutputTemperature(scales.getOutputTemperature());
+            return true;
+        } else {
+            return false;
         }
     }
 }
