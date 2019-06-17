ce05bfa07848744627125bdbd80f5e50f465131f
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-17 22:36:28 +0700

    Add files via upload
    the correcting of cod by remarks of 13.06.19

--- Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
@@ -1,10 +1,12 @@
 package ru.academits.ikonnikov.temperature;
 
+import ru.academits.ikonnikov.temperature.gui.Scales;
 import ru.academits.ikonnikov.temperature.gui.View;
 
 public class Temperature {
     public static void main(String[] args) {
-        View view = new View();
+        Scales scales = new Scales();
+        View view = new View(scales);
         view.run();
         view.setVisible(true);
     }
