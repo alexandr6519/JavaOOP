ca503f93e110fca0a4115aaeb3ca66e186967c35
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-24 17:11:13 +0700

    Add files via upload
    the correcting of cod by using of interface

--- Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
@@ -1,11 +1,14 @@
 package ru.academits.ikonnikov.temperature;
 
-import ru.academits.ikonnikov.temperature.gui.Scales;
+import ru.academits.ikonnikov.temperature.classes.*;
 import ru.academits.ikonnikov.temperature.gui.View;
 
 public class Temperature {
     public static void main(String[] args) {
-        Scales scales = new Scales();
+        Scales[] scales = new Scales[3];
+        scales[0] = new ScaleCelsius();
+        scales[1] = new ScaleFahrenheit();
+        scales[2] = new ScaleKelvin();
         View view = new View(scales);
         view.run();
         view.setVisible(true);
