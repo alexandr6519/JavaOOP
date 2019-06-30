6e38058094f15da0db61eb3141c3ef958c25116b
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2019-06-30 20:02:53 +0700

    Add files via upload
    the change of creation of massiv

--- Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
+++ Temperature/src/ru.academits.ikonnikov.temperature/Temperature.java
@@ -5,12 +5,12 @@ import ru.academits.ikonnikov.temperature.gui.View;
 
 public class Temperature {
     public static void main(String[] args) {
-        Scales[] scales = new Scales[3];
-        scales[0] = new ScaleCelsius();
-        scales[1] = new ScaleFahrenheit();
-        scales[2] = new ScaleKelvin();
+        Scales[] scales = new Scales[]{new ScaleCelsius(), new ScaleFahrenheit(), new ScaleKelvin()};
+
         View view = new View(scales);
+
         view.run();
+
         view.setVisible(true);
     }
 }
